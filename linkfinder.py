a48acdf5015ca896622125d8f14a9407d772af99
Elie07 <79369230+ElieGonzales@users.noreply.github.com>
2022-12-01 15:27:25 -0500

    Added main file

---
+++ linkfinder.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pyautogui as pag
+from time import sleep
+
+print('this program will get you all the links in a website')
+website = input('enter a website (include https://): ')
+openlinks = False
+platform = input("please input a platform (Chrome/Firefox/Edge): ")
+dotext = input('do you want the shown text as well? [Y/N]: ')
+
+if platform.lower() == "chrome":
+    driver = webdriver.Chrome()
+elif platform.lower() == "edge":
+    driver = webdriver.Edge()
+elif platform.lower() == "firefox":
+    driver = webdriver.Firefox()
+
+driver.get(website)
+links = driver.find_elements(By.XPATH,"//*[@href]")
+links = list(dict.fromkeys(links))
+finstr = ''
+finlinks = []
+for link in links:
+    print(link.get_attribute('href'), "as" ,link.text)
+    if openlinks:
+        pag.hotkey('ctrl','t')
+        pag.typewrite(link.get_attribute('href'))
+        pag.hotkey('return')
+        sleep(0.5)
+    if dotext:
+        finlinks.append(link.text, "=" , link.get_attribute('href'))
+    else:    
+        finlinks.append(link.get_attribute('href'))
+
+with open('links.txt','w') as file:
+    for i in finlinks:
+        finstr = finstr + i
+    file.write(finstr)
+driver.close()
