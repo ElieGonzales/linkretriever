cc861941a35fabd4c8a008c4ce5880f186e4ba73
Elie07 <79369230+ElieGonzales@users.noreply.github.com>
2022-12-01 16:12:56 -0500

    Update linkretriever.py
    closes the window if an error happens.

--- linkretriever.py
+++ linkretriever.py
@@ -22,25 +22,27 @@ else:
     print("please use one of the following platforms: chrome, edge, firefox")
 
 driver.get(website)
-links = driver.find_elements(By.XPATH,"//*[@href]")
-links = list(dict.fromkeys(links))
-finstr = ''
-finlinks = []
-for link in links:
-    print(link.get_attribute('href'), "as" ,link.text)
-    if openlinks:
-        pag.hotkey('ctrl','t')
-        pag.typewrite(link.get_attribute('href'))
-        pag.hotkey('return')
-        sleep(0.5)
-    if dotext.lower() == 'y' or dotext.lower() == 'yes':
-        textlink = link.text + " = " + link.get_attribute('href')
-        finlinks.append(textlink)
-    else:    
-        finlinks.append(link.get_attribute('href'))
+try:   
+    links = driver.find_elements(By.XPATH,"//*[@href]")
+    links = list(dict.fromkeys(links))
+    finstr = ''
+    finlinks = []
+    for link in links:
+        print(link.get_attribute('href'), "as" ,link.text)
+        if openlinks:
+            pag.hotkey('ctrl','t')
+            pag.typewrite(link.get_attribute('href'))
+            pag.hotkey('return')
+            sleep(0.5)
+        if dotext.lower() == 'y' or dotext.lower() == 'yes':
+            textlink = link.text + " = " + link.get_attribute('href')
+            finlinks.append(textlink)
+        else:    
+            finlinks.append(link.get_attribute('href'))
 
-with open('links.txt','w') as file:
-    for i in finlinks:
-        finstr = finstr + i + '\n'
-    file.write(finstr)
-driver.close()
+    with open('links.txt','w') as file:
+        for i in finlinks:
+            finstr = finstr + i + '\n'
+        file.write(finstr)
+finally:        
+    driver.close()
